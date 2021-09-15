c3b2cc63846e83c136159111bbf0db5d6c93fe14
Sorawit Suriyakarn <thepsint@gmail.com>
2021-09-15 22:34:38 +0700

    Some example with contract creation

--- bevm/bevm.py
+++ bevm/bevm.py
@@ -10,21 +10,32 @@ from eth.constants import BLANK_ROOT_HASH
 
 
 STATE_ROOT = b'BEVM_STATE_ROOT'
+MIN_GAS_PRICE = b'BEVM_MIN_GAS_PRICE'
 ACTION_COUNT = b'BEVM_ACTION_COUNT'
 ZERO_ADDRESS = b'\x00' * 20
 
 
 class BEVM:
-    def __init__(self, db_path=None, chain_id=494):
+    def __init__(self, db_path=None, chain_id=494, default_min_gas_price=10**9):
         '''Initialize a new BEVM instance.'''
         self.db = LevelDB(db_path) if db_path is not None else AtomicDB()
         self.chain_id = chain_id
+        self.default_min_gas_price = default_min_gas_price
 
     @property
     def state_root(self):
         '''Return the currently stored state root.'''
         db = HashTrie(self.db)
         return db.get(STATE_ROOT) if db.exists(STATE_ROOT) else BLANK_ROOT_HASH
+
+    @property
+    def min_gas_price(self):
+        '''Return the current gas price.'''
+        db = HashTrie(self.db)
+        return (
+            int.from_bytes(db.get(MIN_GAS_PRICE), 'big')
+            if db.exists(MIN_GAS_PRICE) else self.default_min_gas_price
+        )
 
     @property
     def action_count(self):
@@ -45,9 +56,16 @@ class BEVM:
         db = AccountDB(self.db, self.state_root)
         return db.get_balance(account)
 
+    def apply_min_gas_price(self, min_gas_price):
+        '''Perform global min gas price update.'''
+        db = HashTrie(self.db)
+        db.set(MIN_GAS_PRICE, min_gas_price.to_bytes(8, 'big'))
+        self._increase_action_count()
+
     def apply_mint(self, account, amount):
         '''Perform native token mining for the given account.'''
         db = AccountDB(self.db, self.state_root)
+        db.lock_changes()
         db.set_balance(account, db.get_balance(account) + amount)
         db.commit(db.record())
         db.persist()
@@ -60,16 +78,20 @@ class BEVM:
             ZERO_ADDRESS, timestamp, self.action_count,
             0, 0, [], self.chain_id,
         )
+        tx = BerlinTransactionBuilder.new_transaction(
+            nonce, gas_price, gas, to, value, data, v, r, s,
+        )
         state = BerlinState(self.db, ctx, self.state_root)
-        # TODO: Handle revert
-        tx = BerlinTransactionBuilder.new_transaction(nonce, gas_price, gas, to, value, data, v, r, s)
+        state.lock_changes()
+        # TODO: Check min gas price
+        # TODO: Handle revert case
         state.apply_transaction(tx)
         state.commit(state.snapshot())
         state.persist()
         self._update_state_root(state.state_root)
         self._increase_action_count()
 
-    def try_transaction(self, timestamp, sender, nonce, gas_price, gas, to, value, data):
+    def try_transaction(self, timestamp, sender, gas_price, gas, to, value, data):
         '''Try applying the transaction the BEVM but discard all state changes.'''
         ctx = ExecutionContext(
             ZERO_ADDRESS, timestamp, self.action_count,
@@ -78,9 +100,10 @@ class BEVM:
         state = BerlinState(self.db, ctx, self.state_root)
         snapshot = state.snapshot()
         try:
+            nonce = state.get_nonce(sender)
             tx = SpoofTransaction(BerlinTransactionBuilder.create_unsigned_transaction(
                 nonce=nonce, gas_price=gas_price, gas=gas, to=to, value=value, data=data,
             ), from_=sender)
-            state.apply_transaction(tx)
+            return state.apply_transaction(tx)
         finally:
             state.revert(snapshot)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from bevm import BEVM
 from eth_account.account import Account
+from eth._utils.address import generate_contract_address
 
 
 ONE_ADDRESS = b'\x11' * 20
@@ -15,10 +16,15 @@ def main():
         'nonce': 0,
         'to': ONE_ADDRESS,
         'gas': 30000,
-        'gasPrice': 1,
+        'gasPrice': bevm.min_gas_price,
         'value': 10**17,
     }
+    data2 = {
+        'chainId': 494, 'data': bytes.fromhex(
+            '6080604052600160005534801561001557600080fd5b5060cd806100246000396000f3fe6080604052348015600f57600080fd5b506004361060325760003560e01c80630dbe671f14603757806326121ff0146051575b600080fd5b603f60005481565b60405190815260200160405180910390f35b603f60006001600080828254606591906072565b9091555050600054919050565b60008219821115609257634e487b7160e01b600052601160045260246000fd5b50019056fea264697066735822122082dffa335e83d98686e32913d895c036794b1e622f2efdd7f765709db7d3502c64736f6c63430008070033'),
+        'nonce': 1, 'to': b'', 'gas': 200000, 'gasPrice': bevm.min_gas_price, 'value': 0, }
     sig = Account.sign_transaction(data, a.key)
+    sig2 = Account.sign_transaction(data2, a.key)
     bevm.apply_mint(bytes.fromhex(a.address[2:]), 10**18)
     bevm.apply_mint(ONE_ADDRESS, 10**18)
     bevm.apply_mint(TWO_ADDRESS, 10**18)
@@ -30,19 +36,29 @@ def main():
         data['value'],
         data['data'],
         v=sig.v, r=sig.r, s=sig.s)
-    bevm.try_transaction(
+    created_address = generate_contract_address(bytes.fromhex(a.address[2:]), 1)
+    bevm.apply_transaction(
+        500, data2['nonce'],
+        data2['gasPrice'],
+        data2['gas'],
+        data2['to'],
+        data2['value'],
+        data2['data'],
+        v=sig2.v, r=sig2.r, s=sig2.s)
+    res = bevm.try_transaction(
         500,
         TWO_ADDRESS,
-        data['nonce'],
-        data['gasPrice'],
-        data['gas'],
-        data['to'],
-        data['value'],
-        data['data']
+        0,
+        100000,
+        created_address,
+        0,
+        bytes.fromhex('0dbe671f')
     )
+    print(res.output.hex())
     print(bevm.get_balance(bytes.fromhex(a.address[2:])))
     print(bevm.get_balance(ONE_ADDRESS))
     print(bevm.get_balance(TWO_ADDRESS))
+    print(bevm.min_gas_price)
     print(bevm.action_count)
 
 
