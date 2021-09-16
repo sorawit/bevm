5ba3bef88be9a23576a6409e3992883cb397f114
Sorawit Suriyakarn <thepsint@gmail.com>
2021-09-16 13:53:48 +0700

    Refactor to codec.py

---
+++ bevm/codec.py
@@ -0,0 +1,32 @@
+ZERO_ADDRESS = b'\x00' * 20
+
+
+def pack_tx(tx):
+    '''Convert tx dictionary into raw bytes.'''
+    assert tx['to'] == b'' or (len(tx['to']) == 20 and tx['to'] != ZERO_ADDRESS)
+    return (
+        tx['nonce'].to_bytes(8, 'big') +
+        (tx['to'] if len(tx['to']) else ZERO_ADDRESS) +
+        tx['value'].to_bytes(16, 'big') +
+        tx['gas'].to_bytes(4, 'big') +
+        tx['gasPrice'].to_bytes(8, 'big') +
+        tx['v'].to_bytes(1, 'big') +
+        tx['r'].to_bytes(32, 'big') +
+        tx['s'].to_bytes(32, 'big') +
+        tx['data']
+    )
+
+
+def unpack_tx(data):
+    '''Convert raw bytes into tx dictionary.'''
+    return {
+        'nonce': int.from_bytes(data[:8], 'big'),
+        'to': data[8:28] if data[8:28] != ZERO_ADDRESS else b'',
+        'value': int.from_bytes(data[28:44], 'big'),
+        'gas': int.from_bytes(data[44:48], 'big'),
+        'gasPrice': int.from_bytes(data[48:56], 'big'),
+        'v': int.from_bytes(data[56:57], 'big'),
+        'r': int.from_bytes(data[57:89], 'big'),
+        's': int.from_bytes(data[89:121], 'big'),
+        'data': data[121:],
+    }
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from bevm import BEVM
+from bevm.codec import pack_tx, unpack_tx
 from eth_account.account import Account
 from eth._utils.address import generate_contract_address
 
@@ -53,37 +54,6 @@ txs = [{
         's': 2844719216955992928291971831696279864260081229602310533148354655207276528596,
     },
 }]
-
-
-def pack_tx(tx):
-    '''Convert tx dictionary into raw bytes.'''
-    assert tx['to'] == b'' or (len(tx['to']) == 20 and tx['to'] != ZERO_ADDRESS)
-    return (
-        tx['nonce'].to_bytes(8, 'big') +
-        (tx['to'] if len(tx['to']) else ZERO_ADDRESS) +
-        tx['value'].to_bytes(16, 'big') +
-        tx['gas'].to_bytes(4, 'big') +
-        tx['gasPrice'].to_bytes(8, 'big') +
-        tx['v'].to_bytes(1, 'big') +
-        tx['r'].to_bytes(32, 'big') +
-        tx['s'].to_bytes(32, 'big') +
-        tx['data']
-    )
-
-
-def unpack_tx(data):
-    '''Convert raw bytes into tx dictionary.'''
-    return {
-        'nonce': int.from_bytes(data[:8], 'big'),
-        'to': data[8:28] if data[8:28] != ZERO_ADDRESS else b'',
-        'value': int.from_bytes(data[28:44], 'big'),
-        'gas': int.from_bytes(data[44:48], 'big'),
-        'gasPrice': int.from_bytes(data[48:56], 'big'),
-        'v': int.from_bytes(data[56:57], 'big'),
-        'r': int.from_bytes(data[57:89], 'big'),
-        's': int.from_bytes(data[89:121], 'big'),
-        'data': data[121:],
-    }
 
 
 def main():
